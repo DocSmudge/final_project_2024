322f70a6360c049de7f371dee39f576758035fd1
DocSmudge <91097394+DocSmudge@users.noreply.github.com>
2024-04-09 17:16:12 -0600

    got routes set up

---
+++ src/routes/pet_profile/+page.svelte
@@ -0,0 +1,4 @@
+<!-- Responsible for routing features, this is the file that gets serves in the browser-->
+
+<h1>Pet Care App HOME</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
