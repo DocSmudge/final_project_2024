b52e5e035cd5616ebc8c57ceecb2e42b42c0a442
DocSmudge <91097394+DocSmudge@users.noreply.github.com>
2024-04-10 12:56:18 -0600

    Main page UI complete, minus reminder functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 	// import rohan from '/rohan.jpg'
-	import Chart from '../components/chart.svelte'
+	import Chart from '../components/chart.svelte';
 </script>
 
 <h1>Pet Care Reminder App</h1>
@@ -21,6 +21,7 @@
 	<h3>Sex: *dynamically insert sex*</h3>
 </div>
 <!--*----------------------------------->
+
 <!--*----------FEEDING DIV------------->
 <div>
 	<h3>Food Instructions</h3>
@@ -28,6 +29,7 @@
 	<p>(insert)PM</p>
 </div>
 <!--*----------------------------------->
+
 <!--*----------MEDICATION DIV------------->
 <div>
 	<h3>Medications</h3>
@@ -38,6 +40,7 @@
 	</ul>
 </div>
 <!--*----------------------------------->
+
 <!--*------------EXERCISE TRACKER DIV-------------->
 <div>
 	<h3>Exercise Tracker</h3>
@@ -51,11 +54,71 @@
 	</li>
 </div>
 <!--*----------------------------------->
+
 <!--*------------WEIGHT TRACKER DIV-------------->
 <div>
 	<h3>Weight Tracker</h3>
 	<input type="text" placeholder="Weight (lbs)" />
 	<button>Add</button>
-    <Chart />
+	<!-- <Chart /> -->
 </div>
 <!--*----------------------------------->
+
+<!--*------------HEALTH TRACKER DIV-------------->
+<div>
+	<h3>Health Tracker</h3>
+	<input type="text" placeholder="Temperature" /> <input type="text" placeholder="Respirations" />
+	<input type="text" placeholder="Pulse" />
+	<button>Add</button>
+	<li>*Insert Date* *Insert Temperature* *Insert Respirations* *Insert Heart Rate*</li>
+	<li>*Insert Date* *Insert Temperature* *Insert Respirations* *Insert Heart Rate*</li>
+</div>
+<!--*----------------------------------->
+
+<!--*------------STOOL TRACKER DIV-------------->
+<h3>Stool Tracker</h3>
+<input type="text" placeholder="Stool Type" />
+<button>Add</button>
+<!-- <Chart /> -->
+<!--*----------------------------------->
+
+<!--*------------MEDICAL TRACKER DIV-------------->
+<div>
+	<h3>Medical Tracker</h3>
+	<li>
+		<!--add input functionality -->
+		Rabies: *Insert Date*
+		<button>Edit</button>
+		<button>Delete</button>
+	</li>
+	<li>
+		<!--add input functionality -->
+		DAPP: *Insert Date*
+		<button>Edit</button>
+		<button>Delete</button>
+	</li>
+	<li>
+		<!--add input functionality -->
+		BORD: *Insert Date*
+		<button>Edit</button>
+		<button>Delete</button>
+	</li>
+	<li>
+		<!--add input functionality -->
+		LEPTO: *Insert Date*
+		<button>Edit</button>
+		<button>Delete</button>
+	</li>
+	<li>
+		<!--add input functionality -->
+		HWP: *Insert Date*
+		<button>Edit</button>
+		<button>Delete</button>
+	</li>
+	<li>
+		<!--add input functionality -->
+		FLEA/TICK: *Insert Date*
+		<button>Edit</button>
+		<button>Delete</button>
+	</li>
+</div>
