89c48c12b04dfb4191e4576c2e086440ac0c5f55
DocSmudge <91097394+DocSmudge@users.noreply.github.com>
2024-04-15 14:22:31 -0600

    added services page and filler content

--- src/routes/pet_services/+page.svelte
+++ src/routes/pet_services/+page.svelte
@@ -1,2 +1,30 @@
 <!-- Responsible for routing features, this is the file that gets serves in the browser-->
-<h1>PET SERVICES</h1>
+<h1>Pet Care Reminder App</h1>
+<nav>
+	<ul>
+		<li><a href="/">Pet Profile</a></li>
+		<li><a href="/pet_services">Pet Services</a></li>
+		<li><a href="/calendar">Calendar</a></li>
+	</ul>
+</nav>
+
+<h1>SERVICES</h1>
+<div>
+    <h3>Veterinarian</h3>
+    <h3>Name: *dynamically insert Dr. Name</h3>
+    <h3>Address: *dynamically insert address</h3>
+    <h3>Phone: *dynamically insert Phone</h3>
+</div>
+<div>
+    <h3>Groomer</h3>
+    <h3>Name: *dynamically insert Dr. Name</h3>
+    <h3>Address: *dynamically insert address</h3>
+    <h3>Phone: *dynamically insert Phone</h3>
+    <h3>Schedule: *dynamically insert Link</h3>
+</div>
+<div>
+    <h3>Daycare</h3>
+    <h3>Name: *dynamically insert Dr. Name</h3>
+    <h3>Address: *dynamically insert address</h3>
+    <h3>Phone: *dynamically insert Phone</h3>
+</div>
