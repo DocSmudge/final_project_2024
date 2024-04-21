0a987d315953b859bddcc768d19efe0120852e8c
DocSmudge <91097394+DocSmudge@users.noreply.github.com>
2024-04-21 09:04:31 -0600

    got exercise and health input functionality + buttons working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,205 +1,259 @@
 <script>
 	import Button from '$lib/components/ui/button/button.svelte';
-
-	// import rohan from '/rohan.jpg'
+	import { onMount } from 'svelte';
 	import Chart from '../components/chart.svelte';
 	import Pet from '../pet.json';
 	//---------------------------------------
-	let inputValue = '';
+
+	$: pet = null;
+	$: petExercises = [];
+	$: petHealth = [];
+	// $: console.log(petExercises);
+
+	onMount(() => {
+		pet = JSON.parse(JSON.stringify(Pet));
+		petExercises = [...pet.exercises];
+		petHealth = [...pet.health];
+	});
+
+	let exerciseInputValue = '';
+	let tempInputValue = '';
+	let respInputValue = '';
+	let pulseInputValue = '';
+	let errorMessage = '';
+
+	const date = new Intl.DateTimeFormat({
+		year: 'numeric',
+		month: '2-digit',
+		day: '2-digit'
+	}).format(new Date());
 
 	function addExercise() {
-		if (inputValue.trim() !== '') {
-			const newExercise = {
-				id: Pet.exercises.length,
-				date: new Date(),
-				time: new Date().getTime(),
-				exercise: inputValue
-			};
-			Pet.exercises.push(newExercise);
-			console.log(Pet);
+		if (!exerciseInputValue) {
+			return;
 		}
-		inputValue = '';
+
+		const newExercise = {
+			id: pet.exercises.length,
+			date,
+			time: '30 minutes',
+			exercise: exerciseInputValue
+		};
+
+		petExercises = [...petExercises, newExercise];
 	}
 	//---------------------------------------
+
 	function deleteExercise(index) {
-		Pet.exercises = [...Pet.exercises.slice(0, index), ...Pet.exercises.slice(index + 1)];
-		console.log(Pet);
+		// console.log(index);
+		petExercises = [...petExercises.slice(0, index), ...petExercises.slice(index + 1)];
+	}
+	//---------------------------------------
+
+	function addHealth() {
+		if (!tempInputValue || !pulseInputValue || !respInputValue) {
+			errorMessage = 'Please fill out all of the fields';
+			return;
+		}
+		const newHealth = {
+			id: pet.health.length,
+			date,
+			temperature: tempInputValue,
+			respirations: respInputValue,
+			pulse: pulseInputValue
+		};
+		petHealth = [...petHealth, newHealth];
+		// console.log(petHealth);
+	}
+	//---------------------------------------
+	function deleteHealth(index) {
+		petHealth = [...petHealth.slice(0, index), ...petHealth.slice(index + 1)];
 	}
 </script>
 
-<main class="container mx-auto bg-slate-400 flex flex-col text-white">
-	<!--*----------DESCRIPTION DIV------------->
-	<h1 class="text-5xl text-center m-10">Pet Profile</h1>
-	<div class="container mx-auto">
-		<!-- <img src={rohan} alt="Rohan"> -->
-		<div class="flex justify-center">
-			<div class="bg-white w-72 h-72 mr-10"></div>
-			<div class="">
-				<h3 class="text-2xl">Breed: {Pet.breed}</h3>
-				<h3 class="text-2xl">Age: {Pet.age}</h3>
-				<h3 class="text-2xl">Sex: {Pet.sex}</h3>
-			</div>
-		</div>
-		<!--*----------------------------------->
-
-		<!--*----------FEEDING DIV------------->
-		<div class="flex justify-center">
-			<div class="bg-black w-1/3 h-40 my-10">
-				<h3 class="text-center py-5">Food Instructions</h3>
-				<p class="text-center">{Pet.feeding.am}</p>
-				<p class="text-center">{Pet.feeding.pm}</p>
+{#if pet}
+	<main class="container mx-auto bg-slate-400 flex flex-col text-white">
+		<!--*----------DESCRIPTION DIV------------->
+		<h1 class="text-5xl text-center m-10">Pet Profile</h1>
+		<div class="container mx-auto">
+			<div class="flex justify-center">
+				<img class="aspect-auto max-w-72" src="/rohan.jpg" alt="Rohan" />
+				<!-- <div class="bg-white w-72 h-72 mr-10"></div> -->
+				<div class="">
+					<h3 class="text-2xl">Breed: {pet.breed}</h3>
+					<h3 class="text-2xl">Age: {pet.age}</h3>
+					<h3 class="text-2xl">Sex: {pet.sex}</h3>
+				</div>
 			</div>
 			<!--*----------------------------------->
 
-			<!--*----------MEDICATION DIV------------->
-			<div class="bg-black w-1/3 h-40 my-10 mx-10">
-				<h3 class="text-center py-5">Medications</h3>
-				<ul>
-					<li class="text-center">
-						{Pet.medications[0].name}
-						{Pet.medications[0].dose}
-						{Pet.medications[0].instructions}
-					</li>
-					<li class="text-center">
-						{Pet.medications[1].name}
-						{Pet.medications[1].dose}
-						{Pet.medications[1].instructions}
-					</li>
-					<li class="text-center">
-						{Pet.medications[2].name}
-						{Pet.medications[2].dose}
-						{Pet.medications[2].instructions}
-					</li>
-				</ul>
-			</div>
-		</div>
-		<!--*----------------------------------->
-
-		<!--*------------EXERCISE TRACKER DIV-------------->
-		<div class="container bg-black w-5/6 h-60 my-10 mx-auto">
-			<h3 class="text-center py-5">Exercise Tracker</h3>
+			<!--*----------FEEDING DIV------------->
 			<div class="flex justify-center">
-				<input
-					class="text-center py-3 mx-5 text-black"
-					type="text"
-					placeholder="Exercise Name"
-					bind:value={inputValue}
-				/>
-				<button on:click={addExercise} class="btn btn-secondary">Add</button>
-			</div>
-			<ul>
-				{#each Pet.exercises as exercise, index}
-					<li class="list-none m-10">
-						<div>
-							<strong>Date:</strong>
-							{exercise.date}, <strong>Time:</strong>
-							{exercise.time}, <strong>Exercise:</strong>
-							{exercise.exercise}
-							<button class="btn btn-secondary mx-5">Edit</button>
-							<button on:click={deleteExercise(index)} class="btn btn-secondary">Delete</button>
-						</div>
-					</li>
-				{/each}
-			</ul>
-		</div>
-		<!--*----------------------------------->
-
-		<!--*------------WEIGHT TRACKER DIV-------------->
-		<div class="container bg-black w-5/6 h-60 my-10 mx-auto">
-			<h3 class="text-center py-5">Weight Tracker</h3>
-			<div class="flex justify-center">
-				<input class="text-center py-3 mx-5" type="text" placeholder="Weight (lbs)" />
-				<button class="btn btn-secondary">Add</button>
-			</div>
-			<!-- <Chart /> -->
-		</div>
-		<!--*----------------------------------->
-
-		<!--*------------HEALTH TRACKER DIV-------------->
-		<div>
-			<div class="container bg-black w-5/6 h-fit my-10 mx-auto">
-				<h3 class="text-center py-5">Health Tracker</h3>
-				<div class="flex justify-center">
-					<input class="text-center py-3 mx-5" type="text" placeholder="Temperature" />
-					<input class="text-center py-3 mx-5" type="text" placeholder="Respirations" />
-					<input class="text-center py-3 mx-5" type="text" placeholder="Pulse" />
-					<button class="btn btn-secondary">Add</button>
+				<div class="bg-black w-1/3 h-40 my-10">
+					<h3 class="text-center py-5">Food Instructions</h3>
+					<p class="text-center">{pet.feeding.am}</p>
+					<p class="text-center">{pet.feeding.pm}</p>
 				</div>
-				<div class="flex flex-col justify-center">
+				<!--*----------------------------------->
+
+				<!--*----------MEDICATION DIV------------->
+				<div class="bg-black w-1/3 h-40 my-10 mx-10">
+					<h3 class="text-center py-5">Medications</h3>
 					<ul>
-						{#each Pet.health as health, index}
-							<li class="list-none m-10">
-								<div>
-									<strong>Date</strong>
-									<strong>Temperature</strong>
-									<strong>Respirations</strong>
-									<strong>Pulse</strong>
-								</div>
-							</li>
-						{/each}
+						<li class="text-center">
+							{pet.medications[0].name}
+							{pet.medications[0].dose}
+							{pet.medications[0].instructions}
+						</li>
+						<li class="text-center">
+							{pet.medications[1].name}
+							{pet.medications[1].dose}
+							{pet.medications[1].instructions}
+						</li>
+						<li class="text-center">
+							{pet.medications[2].name}
+							{pet.medications[2].dose}
+							{pet.medications[2].instructions}
+						</li>
 					</ul>
 				</div>
 			</div>
-		</div>
-		<!--*----------------------------------->
+			<!--*----------------------------------->
 
-		<!--*------------STOOL TRACKER DIV-------------->
-		<div>
-			<div class="container bg-black w-5/6 h-fit my-10 mx-auto p-5">
-				<h3 class="text-center py-5">Stool Tracker</h3>
-				<div class="flex justify-center m-10">
-					<input class="text-center py-3 mx-5" type="text" placeholder="Stool Type" />
+			<!--*------------EXERCISE TRACKER DIV-------------->
+			<div class="container bg-black w-5/6 h-60 my-10 mx-auto">
+				<h3 class="text-center py-5">Exercise Tracker</h3>
+				<div class="flex justify-center">
+					<input
+						class="text-center py-3 mx-5 text-black"
+						type="text"
+						placeholder="Exercise Name"
+						bind:value={exerciseInputValue}
+					/>
+					<button on:click={addExercise} class="btn btn-secondary">Add</button>
+				</div>
+				<ul>
+					{#each petExercises as e, i}
+						<li>
+							<div>
+								<strong>Date:</strong>
+								{e.date}, <strong>Time:</strong>
+								{e.time}, <strong>Exercise:</strong>
+								{e.exercise}
+								<button class="btn btn-secondary mx-5">Edit</button>
+								<button on:click={() => deleteExercise(i)} class="btn btn-secondary">Delete</button>
+							</div>
+						</li>
+					{/each}
+				</ul>
+			</div>
+			<!--*----------------------------------->
+
+			<!--*------------WEIGHT TRACKER DIV-------------->
+			<div class="container bg-black w-5/6 h-60 my-10 mx-auto">
+				<h3 class="text-center py-5">Weight Tracker</h3>
+				<div class="flex justify-center">
+					<input class="text-center py-3 mx-5" type="text" placeholder="Weight (lbs)" />
 					<button class="btn btn-secondary">Add</button>
 				</div>
+				<!-- <Chart /> -->
 			</div>
-		</div>
-		<!-- <Chart /> -->
-		<!--*----------------------------------->
+			<!--*----------------------------------->
 
-		<!--*------------MEDICAL TRACKER DIV-------------->
-		<div class="container bg-black w-5/6 h-fit my-10 mx-auto p-5">
-			<h3 class="text-center py-5">Medical Tracker</h3>
+			<!--*------------HEALTH TRACKER DIV-------------->
 			<div>
-				<li class="list-none m-10">
-					<!--add input functionality -->
-					Rabies: *Insert Date*
-					<button class="btn btn-secondary m-2">Edit</button>
-					<button class="btn btn-secondary">Delete</button>
-				</li>
-			</div>
-			<li class="list-none m-10">
-				<!--add input functionality -->
-				DAPP: *Insert Date*
-				<button class="btn btn-secondary">Edit</button>
-				<button class="btn btn-secondary">Delete</button>
-			</li>
-			<li class="list-none m-10">
-				<!--add input functionality -->
-				BORD: *Insert Date*
-				<button class="btn btn-secondary">Edit</button>
-				<button class="btn btn-secondary">Delete</button>
-			</li>
-			<li class="list-none m-10">
-				<!--add input functionality -->
-				LEPTO: *Insert Date*
-				<button class="btn btn-secondary">Edit</button>
-				<button class="btn btn-secondary">Delete</button>
-			</li>
-			<li class="list-none m-10">
-				<!--add input functionality -->
-				HWP: *Insert Date*
-				<button class="btn btn-secondary">Edit</button>
-				<button class="btn btn-secondary">Delete</button>
-			</li>
-			<li class="list-none m-10">
-				<!--add input functionality -->
-				FLEA/TICK: *Insert Date*
-				<button class="btn btn-secondary">Edit</button>
-				<button class="btn btn-secondary">Delete</button>
-			</li>
-		</div>
-	</div>
-</main>
+				<div class="container bg-black w-5/6 h-fit my-10 mx-auto">
+					<h3 class="text-center py-5">Health Tracker</h3>
+					<div class="flex justify-center">
+						<input
+							class="text-center py-3 mx-5 text-black"
+							type="text"
+							placeholder="Temperature"
+							bind:value={tempInputValue}
+						/>
+						<input
+							class="text-center py-3 mx-5 text-black"
+							type="text"
+							placeholder="Respirations"
+							bind:value={respInputValue}
+						/>
+						<input
+							class="text-center py-3 mx-5 text-black"
+							type="text"
+							placeholder="Pulse"
+							bind:value={pulseInputValue}
+						/>
+						<button on:click={addHealth} class="btn btn-secondary">Add</button>
+					</div>
+					<div class="flex flex-col justify-center">
+						<ul>
+							<div>
+								<strong>Date</strong>
+								<strong>Temperature</strong>
+								<strong>Respirations</strong>
+								<strong>Pulse</strong>
+							</div>
+							{#if errorMessage}
+								<p class="text-red-600">{errorMessage}</p>
+							{/if}
+							{#each petHealth as health, index}
+								<li class="list-none m-10">
+									<div class="flex">
+										<p class="mr-2">{health.temperature} degrees</p>
+										<p class="mr-2">{health.respirations} breaths per minute</p>
+										<p class="mr-2">{health.pulse} beats per minute</p>
 
-<!-- Todo: Figure out why my UI is taking forever to update when I add a new object to my Pet.exercise object  -->
+										<button class="btn btn-secondary mx-5">Edit</button>
+										<button on:click={() => deleteHealth(index)} class="btn btn-secondary"
+											>Delete</button
+										>
+									</div>
+								</li>
+							{/each}
+						</ul>
+					</div>
+				</div>
+			</div>
+			<!--*----------------------------------->
+
+			<!--*------------STOOL TRACKER DIV-------------->
+			<div>
+				<div class="container bg-black w-5/6 h-fit my-10 mx-auto p-5">
+					<h3 class="text-center py-5">Stool Tracker</h3>
+					<div class="flex justify-center m-10">
+						<input class="text-center py-3 mx-5" type="text" placeholder="Stool Type" />
+						<button class="btn btn-secondary">Add</button>
+					</div>
+				</div>
+			</div>
+			<!-- <Chart /> -->
+			<!--*----------------------------------->
+
+			<!--*------------MEDICAL TRACKER DIV-------------->
+			<div class="container bg-black w-5/6 h-fit my-10 mx-auto p-5">
+				<h3 class="text-center py-5">Medical Tracker</h3>
+				<!-- <div>
+					<ul>
+						{#each pet.vaccines as vaccines}
+					
+						<li>
+							 <div>
+								<p>{vaccines.type}</p>
+								<p>{vaccines.date}</p>
+							</div>
+						</li>
+					</ul>
+					<!-- <li class="list-none m-10">
+						<!--add input functionality -->
+				<!-- Rabies: *Insert Date*
+						<button class="btn btn-secondary m-2">Edit</button>
+						<button class="btn btn-secondary">Delete</button>
+					</li> -->
+				<!-- </div> -->
+			</div>
+		</div>
+	</main>
+{/if}
+
+<!-- Todo: Figure out why my UI is taking forever to update when I add a new object to my pet.exercise object  -->
 <!-- Todo: Figure out how to shorten the "date" info on exercise tracker  -->
